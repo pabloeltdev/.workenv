b8725fde055790fb5fa1d194b57fe0a98dc2508a
Pablo E. L. Targino <pabloeltdev@proton.com>
2024-07-15 22:28:40 -0300

    Fix open folds cmd

--- neovim/.config/nvim/lua/user/autocmds.lua
+++ neovim/.config/nvim/lua/user/autocmds.lua
@@ -4,22 +4,22 @@ local M = {}
 -- function to create a list of commands and convert them to autocommands
 -------- This function is taken from https://github.com/norcalli/nvim_utils
 function M.nvim_create_augroups(definitions)
-  for group_name, definition in pairs(definitions) do
-    api.nvim_command('augroup ' .. group_name)
-    api.nvim_command('autocmd!')
-    for _, def in ipairs(definition) do
-      local command = table.concat(vim.tbl_flatten { 'autocmd', def }, ' ')
-      api.nvim_command(command)
-    end
-    api.nvim_command('augroup END')
-  end
+	for group_name, definition in pairs(definitions) do
+		api.nvim_command("augroup " .. group_name)
+		api.nvim_command("autocmd!")
+		for _, def in ipairs(definition) do
+			local command = table.concat(vim.iter({ "autocmd", def }):flatten():totable(), " ")
+			api.nvim_command(command)
+		end
+		api.nvim_command("augroup END")
+	end
 end
 
 local autoCommands = {
-  -- other autocommands
-  open_folds = {
-    { "BufReadPost,FileReadPost", "*", "normal zR" }
-  }
+	-- other autocommands
+	open_folds = {
+		{ "BufReadPost,FileReadPost", "*", "normal zR" },
+	},
 }
 
 M.nvim_create_augroups(autoCommands)
