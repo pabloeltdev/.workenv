6db47f9bd304d66325896fd4763be4a1445b1ccf
Pablo E. L. Targino <pabloeltdev@proton.com>
2024-11-06 21:41:31 -0300

    Add keymaps in init

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -2,7 +2,7 @@ local vim = vim
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-require "user.options"
-require "user.plugins"
-require "user.autocmds"
-
+require("user.options")
+require("user.plugins")
+require("user.autocmds")
+require("user.keymaps")
